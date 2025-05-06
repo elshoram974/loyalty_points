7b4f2b95aa290d29b68c70fe62802acb7ddea698
Your Name <you@example.com>
2025-05-06 15:13:32 +0300

    feat(ui): enhance promo code section with AddNewBarcodeWidget and localization improvements
    - Added `AddNewBarcodeWidget` with circular button and label to guide adding new barcodes
    - Updated `AbusingCode` widget to include new step and adjust layout/padding
    - Improved Arabic translation for promo code guide title
    - Cleaned up unused constants import in `AbusingCodeSteps`

--- lib/features/dashboard/presentation/widgets/home_widgets/abusing_code_steps.dart
+++ lib/features/dashboard/presentation/widgets/home_widgets/abusing_code_steps.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 import '../../../../../core/utils/constants/app_color.dart';
-import '../../../../../core/utils/constants/app_constants.dart';
 
 class AbusingCodeSteps extends StatelessWidget {
   const AbusingCodeSteps(
