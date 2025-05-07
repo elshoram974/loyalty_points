13330889d1a1e2029a92310bf7431e484ea988a6
Mohammed Riyad <riyadm2001@gmail.com>
2025-05-07 16:00:01 +0300

    feat(dashboard): add real-time user data stream to DashboardController
    - Introduced `getUserData()` in DashboardController to listen for real-time updates using Status<UserModel> stream
    - Added `updateHomeUser` GetBuilder ID for efficient UI updates
    - Refactored `isLoadingUserData` and `user` state management

--- lib/features/dashboard/presentation/controller/dashboard_controller.dart
+++ lib/features/dashboard/presentation/controller/dashboard_controller.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:get/get.dart';
 
 import '../../../../app_info.dart';
+import '../../../../core/status/status.dart';
 import '../../../../core/utils/config/locale/local_lang.dart';
 import '../../../../core/utils/config/routes/routes.dart';
 import '../../../../core/utils/constants/app_strings.dart';
@@ -10,13 +11,16 @@ import '../../../../core/utils/functions/handle_response_in_controller.dart';
 import '../../../../core/utils/functions/show_my_dialog.dart';
 import '../../../../core/utils/functions/show_my_snack_bar.dart';
 import '../../../../core/utils/services/push_notification_service.dart';
+import '../../../auth/data/models/user_model.dart';
 import '../../domain/repositories/home_repositories.dart';
 
 const int _initSelectedPage = 0;
 
 abstract class DashboardController extends GetxController {
   DashboardController();
-  bool get isLoading;
+  bool get isLoadingUserData;
+
+  UserModel get user;
 
   int get selectedScreen;
   void changeHomeScreen(int selected);
@@ -24,7 +28,9 @@ abstract class DashboardController extends GetxController {
   Future<void> getAllData();
 
   Future<void> updateFCMToken();
-  
+
+  Future<void> getUserData();
+
   Future<void> logOut();
 
   void onPopInvoked();
@@ -34,10 +40,15 @@ class DashboardControllerImp extends DashboardController {
   DashboardControllerImp(this.repo);
   final HomeRepositories repo;
 
-  bool _isLoading = false;
+  bool _isLoadingUserData = false;
 
   @override
-  bool get isLoading => _isLoading;
+  bool get isLoadingUserData => _isLoadingUserData;
+
+  UserModel? _user;
+
+  @override
+  UserModel get user => _user ?? UserModel.empty();
 
   int _selectedScreen = _initSelectedPage;
   @override
@@ -60,9 +71,11 @@ class DashboardControllerImp extends DashboardController {
     await Future.wait(
       [
         updateFCMToken(),
+        getUserData(),
       ],
     );
   }
+
   @override
   Future<void> updateFCMToken() async {
     return handleResponseInController<void>(
@@ -73,31 +86,49 @@ class DashboardControllerImp extends DashboardController {
     );
   }
 
+  @override
+  Future<void> getUserData() async {
+    Status<UserModel>? realStatus;
+    _isLoadingUserData = true;
+    update([AppString.updateHomeUser]);
+
+    repo.getUserData().listen(
+      (status) {
+        if (status is Success<UserModel>) {
+          realStatus ??= status;
+          _user = status.data;
+          _isLoadingUserData = true;
+          update([AppString.updateHomeUser]);
+        } else {
+          realStatus = status;
+        }
+      },
+      onDone: () {
+        if (realStatus is Failure<UserModel>) {
+          handleFailureStatus(realStatus as Failure<UserModel>);
+        }
+      },
+    );
+  }
 
   @override
   Future<void> logOut() async {
     return ShowMyDialog.loading(
       () async => await handleResponseInController<void>(
-          status: await repo.logout(),
-          onSuccess: (_) {
-            Get.offAllNamed(AppRoute.login);
-          },
-        )
+        status: await repo.logout(),
+        onSuccess: (_) {
+          Get.offAllNamed(AppRoute.login);
+        },
+      ),
     );
   }
-
-
-
-
-
-
-
 
   DateTime _back = DateTime.now();
   @override
   void onPopInvoked() {
-
-    if(_selectedScreen != _initSelectedPage) return changeHomeScreen(_initSelectedPage);
+    if (_selectedScreen != _initSelectedPage) {
+      return changeHomeScreen(_initSelectedPage);
+    }
 
     if (DateTime.now().difference(_back) < const Duration(seconds: 2)) {
       if (!AppInfo.isDebugMode) exit(0);
