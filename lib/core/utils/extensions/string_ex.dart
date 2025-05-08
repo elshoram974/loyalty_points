a29d43921edaf2595c8113ac55630c1589515be0
Your Name <you@example.com>
2025-05-08 18:18:15 +0300

    fix(string extension): prevent crash when generating abbreviation from empty string

--- lib/core/utils/extensions/string_ex.dart
+++ lib/core/utils/extensions/string_ex.dart
@@ -9,6 +9,7 @@ extension StringHelper on String {
   TextDirection get direction => isRTL ? TextDirection.rtl : TextDirection.ltr;
 
   String get nameAbbreviation {
+    if(trim().isEmpty) return '';
     final List<String> stringList = List.unmodifiable(trim().split(' '));
     String abbreviation = stringList.firstOrNull?.substring(0,1).toUpperCase() ?? '';
     if(stringList.length > 1){
