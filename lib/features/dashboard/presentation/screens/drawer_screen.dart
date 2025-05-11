99a53c56810d9d239b88429a73d261ec9bb7c085
Mohammed Riyad <riyadm2001@gmail.com>
2025-05-11 15:14:07 +0300

    feat: link drawer items to dashboard tabs navigation
    - Connected drawer items "Points History" and "Redeem Points" to their corresponding dashboard tabs using `DashboardTabsEnum`.
    - Added placeholders for the "Profile" drawer item action.
    - Improved consistency by explicitly defining `onTap` for all drawer buttons.

--- lib/features/dashboard/presentation/screens/drawer_screen.dart
+++ lib/features/dashboard/presentation/screens/drawer_screen.dart
@@ -4,13 +4,12 @@ import 'package:get/get.dart';
 import '../../../../core/utils/config/controller/config_controller.dart';
 import '../../../../core/utils/config/locale/local_lang.dart';
 import '../../../../core/utils/constants/app_constants.dart';
+import '../../../../core/utils/types/dashboard_tabs.dart';
 import '../controller/dashboard_controller.dart';
 import '../widgets/drawer_widgets/tile_button_widget.dart';
 
 class HomeDrawer extends GetView<DashboardController> {
-  const HomeDrawer({
-    super.key,
-  });
+  const HomeDrawer({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -23,20 +22,28 @@ class HomeDrawer extends GetView<DashboardController> {
           TileButtonWidget(
             icon: Icons.person_outline_outlined,
             title: localeLang(context).profile,
+            onTap: () {},
           ),
           TileButtonWidget(
             icon: Icons.stars_outlined,
             title: localeLang(context).pointsHistory,
+            onTap: () {
+              controller.changeHomeScreen(DashboardTabsEnum.points.index);
+            },
           ),
           TileButtonWidget(
             icon: Icons.sync_outlined,
             title: localeLang(context).redeemPoints,
+            onTap: () {
+              controller.changeHomeScreen(DashboardTabsEnum.redeemPoints.index);
+            },
           ),
           const Spacer(flex: 5),
           TileButtonWidget(
             icon: Icons.language_outlined,
             title: localeLang(context).changeLanguageTo(
-                Get.find<ConfigController>().alternateLocale.languageName),
+              Get.find<ConfigController>().alternateLocale.languageName,
+            ),
             onTap: Get.find<ConfigController>().toggleLanguage,
           ),
           TileButtonWidget(
