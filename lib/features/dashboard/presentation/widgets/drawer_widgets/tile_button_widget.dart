af555083fcba20cd4b61c87c3a37f29cb8a3443f
Your Name <you@example.com>
2025-06-22 16:08:22 +0300

    feat: enhance drawer tile buttons with card design and color customization
    - Updated `TileButtonWidget` to use `Card` wrapping for improved UI consistency.
    - Added support for custom background color via `color` parameter.
    - Introduced optional `trailingIcon` with default value to allow more flexible usage.
    - Applied padding and rounded corners using `AppConst` constants.
    - Added new constant `AppColor.logout` to be used in logout tile or other soft-colored areas.

--- lib/features/dashboard/presentation/widgets/drawer_widgets/tile_button_widget.dart
+++ lib/features/dashboard/presentation/widgets/drawer_widgets/tile_button_widget.dart
@@ -2,30 +2,46 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:get/get.dart';
 
+import '../../../../../core/utils/constants/app_constants.dart';
+
 class TileButtonWidget extends StatelessWidget {
   const TileButtonWidget({
     super.key,
     required this.title,
     this.onTap,
     this.icon,
+    this.color = Colors.white,
+    this.trailingIcon = Icons.arrow_forward_ios_outlined,
   });
   final String title;
   final IconData? icon;
+  final IconData? trailingIcon;
+  final Color color;
   final void Function()? onTap;
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      onTap: onTap == null
-          ? null
-          : () {
-              Get.back();
-              onTap!();
-            },
-      titleTextStyle:
-          context.textTheme.titleMedium?.copyWith(color: Colors.white),
-      title: Text(title),
-      trailing: icon == null ? null : FaIcon(icon, color: Colors.white),
+    return Card(
+      margin: const EdgeInsets.symmetric(vertical: AppConst.paddingSmall),
+      color: color,
+      child: ListTile(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+        onTap: onTap == null
+            ? null
+            : () {
+                Get.back();
+                onTap!();
+              },
+        titleTextStyle: context.textTheme.titleMedium,
+        title: Text(title),
+        leading: icon == null ? null : FaIcon(icon),
+        trailing: trailingIcon == null
+            ? null
+            : Icon(
+                trailingIcon,
+                size: 16,
+              ),
+      ),
     );
   }
 }
