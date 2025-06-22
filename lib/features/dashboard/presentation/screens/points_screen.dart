9b60b0d4bccd48e7516a1dca3be41b9d5d76068c
Your Name <you@example.com>
2025-06-22 16:10:58 +0300

    chore: remove unused import of PointsBalanceWidget from orders and points screens
    - Cleaned up `points_screen.dart` and `orders_screen.dart` by removing redundant import of `points_balance_widget.dart` after the widget was commented out.

--- lib/features/dashboard/presentation/screens/points_screen.dart
+++ lib/features/dashboard/presentation/screens/points_screen.dart
@@ -4,7 +4,6 @@ import 'package:get/get.dart';
 import '../../../../core/shared/custom_loading.dart';
 import '../../../../core/shared/custom_scaffold.dart';
 import '../../../../core/shared/my_sliver_pinned_tabs.dart';
-import '../../../../core/shared/points_balance_widget.dart';
 import '../../../../core/utils/config/locale/local_lang.dart';
 import '../../../../core/utils/constants/app_constants.dart';
 import '../../../../core/utils/types/points_status_enum.dart';
